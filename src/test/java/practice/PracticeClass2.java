aaba088caf21fe09d89b3cf0f6d8ae84c999fad1
rambhavanprajapati <ram.prajapati15796@gmail.com>
2023-03-10 17:57:33 +0530

    key

--- src/test/java/practice/PracticeClass2.java
+++ src/test/java/practice/PracticeClass2.java
@@ -7,6 +7,10 @@ public class PracticeClass2 {
 	@Test
 	public void test()
 	{
+		String url = System.getProperty("url");
+		System.out.println(url);
+		String b = System.getProperty("browser");
+		System.out.println(b);
 		System.out.println("test2");
 	}
 
